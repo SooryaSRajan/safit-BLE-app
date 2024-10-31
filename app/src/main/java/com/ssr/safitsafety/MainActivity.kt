b03f1ff2e46b416d1f4f4a927f9949ba486f4351
sooryasrajan <soorya.s27@gmail.com>
2024-10-31 19:08:12 +0530

    Added permission checks

--- app/src/main/java/com/ssr/safitsafety/MainActivity.kt
+++ app/src/main/java/com/ssr/safitsafety/MainActivity.kt
@@ -2,7 +2,9 @@ package com.ssr.safitsafety
 
 import android.Manifest
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -11,15 +13,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.ssr.safitsafety.navigation.NavigationStack
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.ssr.safitsafety.navigation.Screen
+import com.ssr.safitsafety.navigation.pages.BluetoothListScreen
+import com.ssr.safitsafety.navigation.pages.DataScreen
 import com.ssr.safitsafety.service.ForegroundService
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var sharedPref: SharedPreferences;
+    private var savedMac = mutableStateOf("")
+    private var arePermissionsAllowed = mutableStateOf(false)
+    companion object {
+        const val PREF_KEY = "SAVED_MAC"
+    }
 
     // Permissions required for location and Bluetooth functionality
     private val permissions = mutableListOf(
@@ -36,7 +58,7 @@ class MainActivity : ComponentActivity() {
     ) { permissions ->
         if (permissions.all { it.value }) {
             // All permissions granted; start the service and register the receiver
-            startForegroundServiceAndRegisterReceiver()
+            arePermissionsAllowed.value = true
         } else {
             Toast.makeText(this, "Permissions are required for Bluetooth scanning and connection", Toast.LENGTH_SHORT).show()
         }
@@ -44,14 +66,55 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+        savedMac.value = sharedPref.getString(PREF_KEY, "").toString()
 
         setContent {
+            val navController = rememberNavController()
+
+            LaunchedEffect(Unit) {
+                checkPermissions()
+                if (savedMac.value.isNullOrEmpty()) {
+                    navController.navigate(route = Screen.Data.route)
+                }
+            }
+
             MaterialTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NavigationStack { checkPermissions() }
+                    if (arePermissionsAllowed.value) {
+                        NavHost(navController = navController, startDestination = Screen.Scan.route) {
+                            composable(route = Screen.Scan.route) {
+                                BluetoothListScreen(navController = navController, sharedPref = sharedPref, onFabClick = {}, savedMac = savedMac)
+                            }
+                            composable(
+                                route = Screen.Scan.route + "?text={text}",
+                                arguments = listOf(
+                                    navArgument("text") {
+                                        type = NavType.StringType
+                                        nullable = true
+                                    }
+                                )
+                            ) {
+                                DataScreen()
+                            }
+                        }
+                    } else {
+                        Text(
+                            text = "Please allow permissions",
+                            style = MaterialTheme.typography.headlineMedium,
+                            modifier = Modifier.padding(bottom = 16.dp)
+                        )
+                        Button(onClick = {checkPermissions() }) {
+                            Text(
+                                text = "Request permissions",
+                                style = MaterialTheme.typography.bodyLarge,
+                                modifier = Modifier.padding(vertical = 4.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -61,7 +124,7 @@ class MainActivity : ComponentActivity() {
         when {
             permissions.all { ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED } -> {
                 // All permissions granted, proceed with service start and receiver registration
-                startForegroundServiceAndRegisterReceiver()
+                arePermissionsAllowed.value = true
             }
             permissions.any { ActivityCompat.shouldShowRequestPermissionRationale(this, it) } -> {
                 // Show rationale dialog explaining why permissions are needed
