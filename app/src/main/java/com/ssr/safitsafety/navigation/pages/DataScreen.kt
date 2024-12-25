0d7a17b78db3b89ea476a1ac2c6e7f6461f50301
sooryasrajan <soorya.s27@gmail.com>
2024-12-25 12:01:36 +0530

    Added edit icon

--- app/src/main/java/com/ssr/safitsafety/navigation/pages/DataScreen.kt
+++ app/src/main/java/com/ssr/safitsafety/navigation/pages/DataScreen.kt
@@ -10,10 +10,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
@@ -118,6 +120,16 @@ fun DataScreen(navController: NavHostController) {
                                 uncheckedBorderColor = MaterialTheme.colorScheme.outline,
                             ),
                             modifier = Modifier.padding(end = 8.dp)
+                        )
+                    }
+                    IconButton(onClick = {
+                        navController.navigate(route = Screen.UserData.route) {
+                            popUpToTop(navController)
+                        }
+                    }) {
+                        Icon(
+                            imageVector = Icons.Filled.Edit,
+                            contentDescription = "Localized description"
                         )
                     }
                 }
